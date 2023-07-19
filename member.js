3c1e57de04a7ee6c37b2ce82d7c8d31b116d8613
Nancy Bronson <nbronson26@users.noreply.github.com>
2023-07-19 01:32:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,25 @@
+function skillsMember() {
+    var skills = [
+        {
+            name: "HTML",
+            level: 4
+        },
+        {
+            name: "CSS",
+            level: 3
+        },
+        {
+            name: "JavaScript",
+            level: 2
+        },
+        {
+            name: "PHP",
+            level: 1
+        },
+        {
+            name: "SQL",
+            level: 1
+        }
+    ];
+    return skills;
+}
